7463e3fb1afad17337e0da5eee8453ca19e62264
Isaac <ac7148126@gmail.com>
2026-02-12 13:48:02 -0600

    feature: add blog module

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
 
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
