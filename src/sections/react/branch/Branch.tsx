81fc32d46304f3ed6c6366ed16ecd4c00b30044d
Isaac <ac7148126@gmail.com>
2026-08-18 17:26:27 -0600

    deploy

--- src/sections/react/branch/Branch.tsx
+++ src/sections/react/branch/Branch.tsx
@@ -16,6 +16,7 @@ const BranchSection = () => {
 
   useEffect(() => {
     const savedBranchId = getStoredBranchId();
+
     if (savedBranchId) {
       getOneBranch(savedBranchId)
         .then((data) => {
